458933c36f484b6d9f68336d608283c53e7c3b29
TBGGLOVE <128962496+TBGGLOVE@users.noreply.github.com>
2023-05-27 17:36:11 +0900

    [level 0] Title: 두 수의 곱, Time: 0.02 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120804. 두 수의 곱/두 수의 곱.java
@@ -0,0 +1,7 @@
+class Solution {
+    int answer1;
+    public int solution(int num1, int num2) {
+        this.answer1 = num1*num2;
+        return answer1; // 이럴거면 걍 return해라 Solution.answer1으로 부를수도 있는데 
+    }
+}
