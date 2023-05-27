df672400842c0e2a3fafb4be6ed2fecc05aa2725
TBGGLOVE <128962496+TBGGLOVE@users.noreply.github.com>
2023-05-27 17:37:48 +0900

    [level 0] Title: 짝수의 합, Time: 0.01 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+        int a=0;
+        for (int i=1; i<=n; i++){
+            if(i%2==0){
+
+            a += i;
+            
+            }
+        }
+    
+    return a;
+    }
+}
